1e642149f88ef6c7f21a47f821b9223c5b182648
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-07-12 18:38:58 +0100

    deploy

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from flask_cors import CORS
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return "Hello"
-CORS(app)
 
-if __name__ == "__main__":
-    app.run()
+
+# if __name__ == "__main__":
+#     app.run()
