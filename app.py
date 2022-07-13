a7d7c9278ea797ec6c9d0fbb4bf068a584b531b4
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-07-13 09:43:06 +0100

    render home

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "<h1>Little URL --> ltl-url</h1>"
-
+def home():
+    return render_template('home.html')
 
 
